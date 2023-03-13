9356363c5dac2680551e8953b95fce23ae7ddb2c
Gilberto Hernandez <gilbertohernandezdev@gmail.com>
2023-03-13 01:09:21 -0300

    Board functionality finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Board from "./Board.jsx";
 
 function App() {
   return (
     <>
       <h1>Tic Tac Toe</h1>
+      <Board />
     </>
   );
 }
 
 export default App;
+
+// TODO: Taking turns
